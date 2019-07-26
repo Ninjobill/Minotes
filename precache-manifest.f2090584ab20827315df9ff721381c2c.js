b7d5b0703a5cc7c05e629b46f65cd0915f5aa367
Billy Todd <billy.todd50@yahoo.com>
2019-07-26 10:34:23 -0700

    Updates

---
+++ precache-manifest.f2090584ab20827315df9ff721381c2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a3b5c84bce16b04f9ec1a83b5adf7a",
+    "url": "/Minotes/index.html"
+  },
+  {
+    "revision": "75e49f0105fe6fe2769f",
+    "url": "/Minotes/static/css/main.65553a91.chunk.css"
+  },
+  {
+    "revision": "023db5ece34f1a07d5ad",
+    "url": "/Minotes/static/js/2.c2944474.chunk.js"
+  },
+  {
+    "revision": "75e49f0105fe6fe2769f",
+    "url": "/Minotes/static/js/main.46a9adf7.chunk.js"
+  },
+  {
+    "revision": "ccc8556d993c97ba6cc5",
+    "url": "/Minotes/static/js/runtime~main.084e3a34.js"
+  }
+]);
